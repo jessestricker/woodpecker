c3b4851a5ed61d7a4a4e2fc0ff0195c4cba1ddaf
Jesse Stricker <jesse.stricker@haw-hamburg.de>
2021-12-13 00:39:47 +0100

    Abort on assertion violation

--- src/woodpecker/util/assert.hpp
+++ src/woodpecker/util/assert.hpp
@@ -17,40 +17,75 @@
 
 #pragma once
 
-// fix std::source_location not being found by CLion
-#ifndef __cpp_consteval
-#define __cpp_consteval
-#endif
-
-#include <source_location>
-#include <stdexcept>
+#include <cstdlib>
 #include <string_view>
 
-#include <spdlog/fmt/fmt.h>
+#include <spdlog/spdlog.h>
 
 namespace wdp {
-  class AssertionViolation : public std::logic_error {
-  public:
-    using std::logic_error::logic_error;
+  struct SourceLocation {
+    const char* file = nullptr;
+    unsigned line = 0;
+    const char* function = nullptr;  // optional, may be nullptr
   };
+}
 
-  namespace details {
-    template <class Fn>
-    void do_assert(const Fn& expr_lambda, std::string_view expr_str, std::source_location src_loc,
-                   std::string_view message = "") {
-      const auto succeeded = expr_lambda();
-      if (!succeeded) [[unlikely]] {
-        auto what_text =
-            fmt::format("assertion violated: {}\nsource: {}:{}", expr_str, src_loc.file_name(), src_loc.line());
-        if (!message.empty()) {
-          what_text += "\nmessage: ";
-          what_text += message;
-        }
-        throw AssertionViolation{what_text};
-      }
+template <>
+struct fmt::formatter<wdp::SourceLocation> : fmt::formatter<string_view> {
+  template <class FormatContext>
+  auto format(const wdp::SourceLocation& src_loc, FormatContext& ctx) {
+    if (src_loc.function != nullptr) {
+      return format_to(ctx.out(), "{}:{}, {}", src_loc.file, src_loc.line, src_loc.function);
     }
+    return format_to(ctx.out(), "{}:{}", src_loc.file, src_loc.line);
   }
+};
+
+#if defined(_MSC_VER)
+#define WDP_PRETTY_FUNCTION __FUNCSIG__
+#elif defined(__clang__) || defined(__GNUC__)
+#define WDP_PRETTY_FUNCTION __PRETTY_FUNCTION__
+#else
+#error unsupported compiler
+#endif
+
+#define WDP_CURRENT_SOURCE_LOCATION         \
+  ::wdp::SourceLocation {                   \
+    __FILE__, __LINE__, WDP_PRETTY_FUNCTION \
+  }
+
+namespace wdp {
+  class Assertion {
+  public:
+    Assertion(const char* expr_str, const SourceLocation& src_loc, const char* message = nullptr)
+        : expr_str_{expr_str}, src_loc_{src_loc}, message_{message} {
+    }
+
+    template <class ExprLambda>
+    void operator()(const ExprLambda& expr_lambda) noexcept {
+      const auto expr_value = expr_lambda();
+      if (expr_value) [[likely]] {
+        return;
+      }
+      log_violation();
+      std::abort();
+    }
+
+  private:
+    const char* expr_str_;
+    SourceLocation src_loc_;
+    const char* message_;  // optional, may be nullptr
+
+    void log_violation() noexcept {
+      spdlog::critical("assertion violated: {}", expr_str_);
+      spdlog::critical("  source: {}", src_loc_);
+      if (message_ != nullptr) {
+        spdlog::critical("  message: {}", message_);
+      }
+      spdlog::shutdown();
+    }
+  };
 }
 
 #define WDP_ASSERT(expr__, ...) \
-  ::wdp::details::do_assert([&]() noexcept { return expr__; }, #expr__, std::source_location::current(), __VA_ARGS__)
+  ::wdp::Assertion{#expr__, WDP_CURRENT_SOURCE_LOCATION, __VA_ARGS__}([&]() noexcept { return expr__; })
